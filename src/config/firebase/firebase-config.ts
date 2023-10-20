f229ba8bb20a9bd97a14bc178bd476c508a5ac8a
SumaiyaMoon <sumaiyaaltafmoon@gmail.com>
2023-10-21 02:03:42 +0500

    configured

--- src/config/firebase/firebase-config.ts
+++ src/config/firebase/firebase-config.ts
@@ -1,21 +1,21 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
+import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCxwQGicGkk-D4-mbSkMuwBpu1cu6cF2WU",
-  authDomain: "moon-lms-software.firebaseapp.com",
-  projectId: "moon-lms-software",
-  storageBucket: "moon-lms-software.appspot.com",
-  messagingSenderId: "708633884973",
-  appId: "1:708633884973:web:9d31c6245640ab475ffba4",
-  measurementId: "G-K1F7M0DEG2",
+  apiKey: "AIzaSyDqNfVDIMGurh43TBNEnpWGuDU8WWeZEg0",
+  authDomain: "mod-b-hackathon.firebaseapp.com",
+  projectId: "mod-b-hackathon",
+  storageBucket: "mod-b-hackathon.appspot.com",
+  messagingSenderId: "1033444623005",
+  appId: "1:1033444623005:web:126580b95f48da01f48a74",
+  measurementId: "G-6QNK4MHDRL",
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
+const analytics = getAnalytics(app);
